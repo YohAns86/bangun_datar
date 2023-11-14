fd072c3e36c49edd520c96f789bbb88a8b681793
USER <yohananangekap@gmail.com>
2023-11-14 14:40:43 +0700

    custom component

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -15,22 +15,34 @@ class HomePage extends StatelessWidget {
       ),
       body: Column(
         children: [
-          Container(
-            margin: EdgeInsets.all(8),
-            padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-            decoration: BoxDecoration(
-                color: Colors.green
-            ),
-            child: Text("Persegi"),
+          CustomMenu(imageAssets: "assets/img_4.png",title: "Persegi"),
+          CustomMenu(imageAssets: "assets/img_2.png",title: "Segitiga samakaki"),
+        ],
+      ),
+    );
+  }
+}
+
+class CustomMenu extends StatelessWidget {
+  const CustomMenu({
+    super.key, required this.imageAssets, required this.title,
+  });
+  final String imageAssets;
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(8),
+      padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+      decoration: BoxDecoration(color: Colors.green),
+      child: Column(
+        children: [
+          Image.asset(
+           imageAssets,
+          height: 100,
           ),
-          Container(
-            margin: EdgeInsets.all(8),
-            padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-            decoration: BoxDecoration(
-                color: Colors.green
-            ),
-            child: Text("Segitiga"),
-          )
+          Text(title),
         ],
       ),
     );
